d079f4805ee59d02128ff767e64c3875c1d7cf47
Leonardo Kremer <168785661+DevLeonardoK@users.noreply.github.com>
2025-12-14 22:07:47 -0300

    Atividade de operacoes matematicas, utilizando casting de numeros

---
+++ java_learn/src/Matematica.java
@@ -0,0 +1,14 @@
+public class Matematica {
+    public static void main(String[] args){
+        int numero1 = 15;
+        int numero2 = 10;
+
+        Double numero1Double = Double.valueOf(numero1);
+        Double numero2Double = (double) numero2;
+
+        System.out.println("A soma dos números: " + (numero1+numero2));
+        System.out.println("A subtração dos números: " + (numero1-numero2));
+        System.out.println("A divisão do n1 por n2: " + (numero1Double/numero2Double) );
+        System.out.println("A multiplicação dos números: " + (numero1*numero2));
+    }
+}
