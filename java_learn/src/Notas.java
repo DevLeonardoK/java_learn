441ec31a7fd47406eefdfcc8f56281bbea8ce94c
Leonardo Kremer <168785661+DevLeonardoK@users.noreply.github.com>
2025-12-14 22:21:10 -0300

    Definir status do aluno com base na nota do input

---
+++ java_learn/src/Notas.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Notas {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Qual sua nota final: ");
+        Double nota = input.nextDouble();
+
+        if (nota >= 7){
+            System.out.println("Aprovado !");
+        }else if(nota >= 5 && nota < 7){
+            System.out.println("Recuperação");
+        }else{
+            System.out.println("Reprovado!");
+        }
+
+    }
+}
